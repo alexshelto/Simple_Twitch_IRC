d50800bb96713017a8a3522604d612c0fc83f806
Alex Shelton <alexandershelton@icloud.com>
2021-06-01 17:11:43 -0400

    more work

--- src/Irc_bot.py
+++ src/Irc_bot.py
@@ -1,8 +1,8 @@
 
 import socket
+import time
 
-# IRC CLIENT irc://irc.chat.twitch.tv:6697
-
+# IRC CLIENT irc://irc.chat.twitch.tv:6667
 server='irc.chat.twitch.tv'
 port=6667
 
@@ -20,26 +20,37 @@ class IRC_BOT:
         '''Function uses the passed in username, oauth, and channel to build a 
         connection to the twitch IRC to get the channel's chat feed'''
 
-        self.irc = socket.socket()
-        self.irc.connect((server, port))
-        self.irc.send(f'PASS {self.oauth}\n'.encode('utf-8'))
-        self.irc.send(f'NICK {self.username}\n'.encode('utf-8'))
-        self.irc.send(f'JOIN {self.channel}\n'.encode('utf-8'))
+        retry_time = 2 # seconds
+        retry_count = 0
+        try:
+            self.irc = socket.socket()
+            self.irc.connect((server, port))
+            self.irc.send(f'PASS {self.oauth}\n'.encode('utf-8'))
+            self.irc.send(f'NICK {self.username}\n'.encode('utf-8'))
+            self.irc.send(f'JOIN {self.channel}\n'.encode('utf-8'))
 
+            return True
+
+        except ConnectionResetError:
+            print(f'Could not connect, trying again in {retry_time} seconds')
+            print('Check that you entered a valid channel name')
+
+            time.sleep(retry_time)
+            retry_time = retry_time ** 2
+            retry_count += 1
+
+            if retry_count == 8:
+                return False
 
     def view_chat(self,channel):
 
         print(f'Attempting to join: {channel}')
-        self.connect_irc(channel)
-        print(self.irc)
+        did_connect = self.connect_irc(channel)
+        if did_connect != True:
+            print('failed to connect')
+            return 
 
         while True:
-            resp = self.irc.recv(2048).decode('utf-8')
-            if resp.startswith('PING'):
-                self.irc.send("PONG\n".encode('utf-8'))
-
-            print(resp)
-            '''
             try:
                 resp = self.irc.recv(2048).decode('utf-8')
                 if resp.startswith('PING'):
@@ -51,7 +62,6 @@ class IRC_BOT:
                 print('\n\nExiting Chat View\n\n')
                 self.irc = None
                 break
-            '''
         
 
     
--- src/main.py
+++ src/main.py
@@ -6,37 +6,6 @@ from IRC_BOT import IRC_BOT
 
 
 
-'''
-
-
-#ircbot = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# IRC CLIENT irc://irc.chat.twitch.tv:6697
-
-#server='irc://irc.chat.twitch.tv'
-server='irc.chat.twitch.tv'
-port=6667
-
-BOT_USERNAME="be9ns"
-OAUTH_TOKEN="oauth:xxycfz8i5f2dqav27mt9chngi705u7"
-
-
-ircbot = socket.socket()
-ircbot.connect((server,port))
-
-ircbot.send(f"PASS {OAUTH_TOKEN}\n".encode('utf-8'))
-ircbot.send(f"NICK {BOT_USERNAME}\n".encode('utf-8'))
-ircbot.send(f"JOIN #nickmercs\n".encode('utf-8'))
-
-
-while True:
-    resp = ircbot.recv(2048).decode('utf-8')
-    if resp.startswith('PING'):
-        ircbot.send("PONG\n".encode('utf-8'))
-    print(resp)
-'''
-
-
-
 options = ['View Chat From Channel', 'View & Log Chat From Channel', 'Exit']
 def menu():
     print('#'* 15 + ' MENU ' + '#' * 15)
@@ -77,9 +46,7 @@ def main() -> int:
     if args.log != False:
         args.log = True
     
-    #def __init__(self, uname, oauth, logging):
     irc = IRC_BOT(CONFIG['BOT_USERNAME'], CONFIG['OAUTH_TOKEN'])
-
 
     # Main Loop
     while True:
